393ee07072fee65a091b25dd430da407448edee3
Enmanuel Domínguez <enmandom@gmail.com>
2026-08-23 21:22:20 -0400

    chore(conove): ensure minSdkVersion 21 compatibility

--- conove/android/app/build.gradle.kts
+++ conove/android/app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.gestura.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 24
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
